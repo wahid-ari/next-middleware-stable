3920b34a4f59a680979fec53239101d43931d014
wahid-ari <wahiid.arii@Gmail.com>
2022-09-16 09:53:11 +0700

    update

--- middleware.js
+++ middleware.js
@@ -1,76 +1,60 @@
 // https://nextjs.org/docs/advanced-features/middleware
 // https://nextjs.org/docs/messages/middleware-upgrade-guide
 
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 // CONDITIONAL STATEMENT
 // https://nextjs.org/docs/advanced-features/middleware#conditional-statements
-// export function middleware(request) {
-//   const url = request.nextUrl.clone()
-//   url.pathname = '/login'
-
-//   const token = request.cookies.get('token')
-//   const response = NextResponse.next()
-
-//   if (request.nextUrl.pathname.startsWith('/admin')) {
-//     // console.log("admin middleware")
-//     // console.log("token :", token)
-//     if (token == undefined) {
-//       return NextResponse.redirect(url)
-//       // return NextResponse.redirect(new URL('/login', request.url))
-//     }
-//     return NextResponse.next()
-//   }
-  
-//   if (request.nextUrl.pathname.startsWith('/logout')) {
-//     // console.log("logout middleware")
-//     // Deleting cookies
-//     response.cookies.delete('token')
-//     response.cookies.delete('username')
-//     response.cookies.clear()
-//     NextResponse.redirect(new URL('/login', request.url))
-//   }
-// return NextResponse.next()
-  
-// }
+export function middleware(request) {
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    const token = request.cookies.get('token');
+    console.log(token)
+    if (!token) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+};
 
 // MATCHER
 // https://nextjs.org/docs/advanced-features/middleware#matcher
-export default async function middleware(request) {
-  const token = await request.cookies.get('token')
-  // console.log(token)
-  // Example function to validate auth
-  // const response = NextResponse.next()
-  // console.log(response)
-  if (token !== undefined) {
-    return NextResponse.next()
-  }
+// export default async function middleware(request) {
+//   const token = await request.cookies.get('token')
 
-  const loginUrl = new URL('/login', request.url)
-  loginUrl.searchParams.set('from', request.nextUrl.pathname)
-  // console.log(loginUrl)
+//   if (token !== undefined) {
+//     return NextResponse.next()
+//   }
 
-  return NextResponse.redirect(loginUrl)
+//   const loginUrl = new URL('/login', request.url)
+//   loginUrl.searchParams.set('from', request.nextUrl.pathname)
 
-  // const url = request.nextUrl.clone()
-  // url.pathname = '/login'
-  // // Getting cookies from the request
-  // const token = request.cookies.get('token')
-  // console.log("Accessing /admin")
-  // console.log("checking token")
-  // if (!token) {
-  //   console.log("no token, redirect to /login")
-  //   return NextResponse.redirect(url)
-  //   // return NextResponse.redirect(new URL('/login', request.url))
-  // }
-  // console.log("Token okey")
-  // console.log("Continue to /admin")
-  // return NextResponse.next()
-}
-// Middleware will be invoked for every route in the app, and a custom matcher can be used to define matching filters.
-// The following is an example for a Middleware that triggers for /about/* and /dashboard/:path*,
-// the custom matcher is defined in an exported config object:
-// Supports both a single string value or an array of matchers
-export const config = {
-  matcher: ['/admin/:path*'],
-}
+//   return NextResponse.redirect(loginUrl)
+// }
+// export const config = {
+//   matcher: ['/admin/:path*'],
+// }
+
+// // MATCHER
+// // https://nextjs.org/docs/advanced-features/middleware#matcher
+// export default function middleware(request) {
+//   // const url = request.nextUrl.clone()
+//   // url.pathname = '/login'
+//   // // Getting cookies from the request
+//   // const token = request.cookies.get('token')
+//   // console.log("Accessing /admin")
+//   // console.log("checking token")
+//   // if (!token) {
+//   //   console.log("no token, redirect to /login")
+//   //   return NextResponse.redirect(url)
+//   //   // return NextResponse.redirect(new URL('/login', request.url))
+//   // }
+//   // console.log("Token okey")
+//   // console.log("Continue to /admin")
+//   // return NextResponse.next()
+// }
+// // Middleware will be invoked for every route in the app, and a custom matcher can be used to define matching filters.
+// // The following is an example for a Middleware that triggers for /about/* and /dashboard/:path*,
+// // the custom matcher is defined in an exported config object:
+// // Supports both a single string value or an array of matchers
+// export const config = {
+//   matcher: ['/admin/:path*'],
+// }
